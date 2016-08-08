a13fa04b53853bf2ee3ee1ac4d90f0d56837ab1b
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2016-08-08 11:57:36 -0700

    Move launcher code (#3)
    * Move DrupalConsole Launcher classes.
    
    * Add InitCommand.
    
    * Remove files.
    
    * Keep/Restore original argv values.
    
    * Inject application root to ConfigurationManager class.
    
    * Add ListCommand class.
    
    * Add loadConfiguration method.
    
    * Add HelpCommand class.
    
    * Add AboutCommand class.
    
    * Update AboutCommand class.
    
    * Update AboutCommand class.
    
    * Add translation files.
    
    * Update translator resources path.
    
    * Pass directory to loadCoreLanguage.
    
    * Pass core language path.
    
    * Remove config directory.

---
+++ src/Utils/ArgvInputReader.php
@@ -0,0 +1,214 @@
+<?php
+
+namespace Drupal\Console\Utils;
+
+use Symfony\Component\Console\Input\ArgvInput;
+
+/**
+ * Class ArgvInputReader.
+ */
+class ArgvInputReader
+{
+    /**
+     * @var array
+     */
+    protected $options;
+
+    /**
+     * @var
+     */
+    protected $input;
+
+    /**
+     * @var array
+     */
+    protected $originalArgvValues;
+
+    /**
+     * ArgvInputReader constructor.
+     */
+    public function __construct()
+    {
+        $this->originalArgvValues = $_SERVER['argv'];
+        $this->options = [];
+        $this->setOptionsFromPlaceHolders();
+        $this->readArgvInputValues();
+    }
+
+    /**
+     * @param array $targetConfig
+     */
+    public function setOptionsFromTargetConfiguration($targetConfig)
+    {
+        $options = [];
+        if (array_key_exists('root', $targetConfig)) {
+            $options['root'] = $targetConfig['root'];
+        }
+        if (array_key_exists('uri', $targetConfig)) {
+            $options['uri'] = $targetConfig['uri'];
+        }
+
+        if (array_key_exists('remote', $targetConfig)) {
+            $this->set('remote', true);
+        }
+
+        $this->setArgvOptions($options);
+    }
+
+    /**
+     * @param array $options
+     */
+    public function setOptionsFromConfiguration($options)
+    {
+        $this->setArgvOptions($options);
+    }
+
+    /**
+     * @param $options
+     */
+    private function setArgvOptions($options)
+    {
+        foreach ($options as $key => $option) {
+            if ($option == 1) {
+                $_SERVER['argv'][] = sprintf('--%s', $key);
+                continue;
+            }
+            if (!empty($option)) {
+                if ($key = 'root') {
+                    foreach ($_SERVER['argv'] as $argvKey => $argv) {
+                        if (strpos($argv, '--'.$key) === 0) {
+                            $argvValue = str_replace('--'.$key.'=', '', $argv);
+                            $_SERVER['argv'][$argvKey] = sprintf('--%s=%s%s', $key, $argvValue, $option);
+                        } else {
+                            $_SERVER['argv'][] = sprintf('--%s=%s', $key, $option);
+                        }
+                    }
+                } else {
+                    $_SERVER['argv'][] = sprintf('--%s=%s', $key, $option);
+                }
+            }
+        }
+        $this->readArgvInputValues();
+    }
+
+    /**
+     * SetPlaceHolderAsOption.
+     */
+    private function setOptionsFromPlaceHolders()
+    {
+        if (count($_SERVER['argv']) > 2
+            && stripos($_SERVER['argv'][1], '@') === 0
+            && stripos($_SERVER['argv'][2], '@') === 0
+        ) {
+            $_SERVER['argv'][1] = sprintf(
+                '--source=%s',
+                substr($_SERVER['argv'][1], 1)
+            );
+
+            $_SERVER['argv'][2] = sprintf(
+                '--target=%s',
+                substr($_SERVER['argv'][2], 1)
+            );
+
+            return;
+        }
+
+        if (count($_SERVER['argv']) > 1 && stripos($_SERVER['argv'][1], '@') === 0) {
+            $_SERVER['argv'][1] = sprintf(
+                '--target=%s',
+                substr($_SERVER['argv'][1], 1)
+            );
+        }
+    }
+
+    /**
+     * ReadArgvInputValues.
+     */
+    private function readArgvInputValues()
+    {
+        $input = new ArgvInput();
+
+        $source = $input->getParameterOption(['--source', '-s'], null);
+        $target = $input->getParameterOption(['--target', '-t'], null);
+        $root = $input->getParameterOption(['--root'], null);
+        $root = (strpos($root, '/') === 0) ? $root : sprintf(
+            '%s/%s',
+            getcwd(),
+            $root
+        );
+        $uri = $input->getParameterOption(['--uri', '-l']) ?: 'default';
+        if ($uri && !preg_match('/^(http|https):\/\//', $uri)) {
+            $uri = sprintf('http://%s', $uri);
+        }
+
+        $this->set('command', $input->getFirstArgument());
+        $this->set('root', $root);
+        $this->set('uri', $uri);
+        $this->set('source', $source);
+        $this->set('target', $target);
+    }
+
+    /**
+     * @param $option
+     * @param $value
+     */
+    private function set($option, $value)
+    {
+        if ($value) {
+            $this->options[$option] = $value;
+
+            return;
+        }
+
+        if (!array_key_exists($option, $this->options)) {
+            unset($this->options[$option]);
+        }
+    }
+
+    /**
+     * @param $option
+     * @param null $value
+     *
+     * @return string
+     */
+    public function get($option, $value = null)
+    {
+        if (!array_key_exists($option, $this->options)) {
+            return $value;
+        }
+
+        return $this->options[$option];
+    }
+
+    /**
+     * @return array
+     */
+    public function getAll()
+    {
+        return $this->options;
+    }
+
+    public function setOptionsAsArgv()
+    {
+        foreach ($this->options as $optionName => $optionValue) {
+            if ($optionName == 'command') {
+                continue;
+            }
+            $optionFound = false;
+            foreach ($_SERVER['argv'] as $key => $argv) {
+                if (strpos($argv, '--'.$optionName) === 0) {
+                    $_SERVER['argv'][$key] = '--'.$optionName.'='.$optionValue;
+                    $optionFound = true;
+                    break;
+                }
+            }
+            if (!$optionFound) {
+                $_SERVER['argv'][] = '--'.$optionName.'='.$optionValue;
+            }
+        }
+    }
+
+    public function restoreOriginalArgvValues() {
+        return $_SERVER['argv'] = $this->originalArgvValues;
+    }
+}
---
+++ src/Utils/ConfigurationManager.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Drupal\Console\Utils;
+
+use Symfony\Component\Yaml\Yaml;
+use Dflydev\DotAccessConfiguration\YamlFileConfigurationBuilder;
+use Dflydev\DotAccessConfiguration\ConfigurationInterface;
+use Symfony\Component\Console\Input\ArgvInput;
+
+/**
+ * Class ConfigurationManager.
+ */
+class ConfigurationManager
+{
+    /**
+     * @var ConfigurationInterface
+     */
+    private $configuration = null;
+
+    /**
+     * @param $applicationRoot
+     */
+    public function loadConfiguration($applicationRoot)
+    {
+        $input = new ArgvInput();
+        $root = $input->getParameterOption(['--root'], null);
+
+        $files = [
+            $applicationRoot.'/../config.yml',
+            $this->getHomeDirectory().'/.console/config.yml',
+            getcwd().'/console/config.yml',
+        ];
+
+        if ($root) {
+            $files[] = $root.'/console/config.yml';
+        }
+
+        $builder = new YamlFileConfigurationBuilder($files);
+
+        $this->configuration = $builder->build();
+    }
+
+    /**
+     * @return ConfigurationInterface
+     */
+    public function getConfiguration()
+    {
+        return $this->configuration;
+    }
+
+    /**
+     * @param $target
+     *
+     * @return array
+     */
+    public function readTarget($target)
+    {
+        if (!$target || !strpos($target, '.')) {
+            return [];
+        }
+
+        $site = explode('.', $target)[0];
+        $env = explode('.', $target)[1];
+
+        $siteFile = sprintf(
+            '%s%s%s.yml',
+            $this->getSitesDirectory(),
+            DIRECTORY_SEPARATOR,
+            $site
+        );
+
+        if (!file_exists($siteFile)) {
+            return [];
+        }
+
+        $targetInformation = Yaml::parse(file_get_contents($siteFile));
+
+        if (!array_key_exists($env, $targetInformation)) {
+            return [];
+        }
+
+        $targetInformation = $targetInformation[$env];
+
+        if (array_key_exists('host', $targetInformation) && $targetInformation['host'] != 'local') {
+            $targetInformation['remote'] = true;
+        }
+
+        return array_merge(
+            $this->configuration->get('application.remote'),
+            $targetInformation
+        );
+    }
+
+    /**
+     * Return the user home directory.
+     *
+     * @return string
+     */
+    public function getHomeDirectory()
+    {
+        if (function_exists('posix_getuid')) {
+            return posix_getpwuid(posix_getuid())['dir'];
+        }
+
+        return rtrim(getenv('HOME') ?: getenv('USERPROFILE'), '/\\');
+    }
+
+    /**
+     * Return the site config directory.
+     *
+     * @return string
+     */
+    private function getSitesDirectory()
+    {
+        return sprintf(
+            '%s%s.console%ssites',
+            $this->getHomeDirectory(),
+            DIRECTORY_SEPARATOR,
+            DIRECTORY_SEPARATOR
+        );
+    }
+}
