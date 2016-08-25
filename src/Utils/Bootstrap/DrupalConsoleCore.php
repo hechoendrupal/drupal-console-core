63785f279729343c6013bfb76ecf201aa0cdc60d
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2016-08-25 01:38:29 -0700

    [console] Add bootstrap class.

---
+++ src/Utils/Bootstrap/DrupalConsoleCore.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Drupal\Console\Utils\Bootstrap;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class DrupalConsoleCore {
+
+    protected $root;
+
+    /**
+     * DrupalConsole constructor.
+     * @param $root
+     */
+    public function __construct($root) {
+        $this->root = $root;
+    }
+
+    public function boot() {
+        $container = new ContainerBuilder();
+        $loader = new YamlFileLoader($container, new FileLocator($this->root));
+        $loader->load($this->root.DRUPAL_CONSOLE_CORE.'/services.yml');
+        if (file_exists($this->root.'/services.yml')) {
+            $loader->load('services.yml');
+        }
+
+        $container->get('console.configuration_manager')
+            ->loadConfiguration($this->root)
+            ->getConfiguration();
+
+        $container->get('console.translator_manager')
+            ->loadCoreLanguage('en', $this->root);
+
+        $container->set(
+            'app.root',
+            $this->root
+        );
+
+        $container->get('console.renderer')
+            ->setSkeletonDirs(
+                [
+                    $this->root.'/templates/',
+                    $this->root.DRUPAL_CONSOLE_CORE.'/templates/'
+                ]
+            );
+
+        return $container;
+    }
+}
