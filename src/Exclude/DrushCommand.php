30d37c68285b988f359d3bc89efb9af93e33f138
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2016-09-08 02:51:54 -0700

    Relocate autowire command registration (#33)
    * [console] Relocate autowire command registration.
    
    * [console] Relocate autowire command registration.

---
+++ src/Exclude/DrushCommand.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\Console\Command\Exclude\DrushCommand.
+ */
+
+namespace Drupal\Console\Command\Exclude;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Command\Command;
+use Drupal\Console\Command\Shared\CommandTrait;
+use Drupal\Console\Utils\ConfigurationManager;
+use Drupal\Console\Utils\ChainQueue;
+use Drupal\Console\Style\DrupalStyle;
+
+class DrushCommand extends Command
+{
+    use CommandTrait;
+
+    /**
+     * @var  ConfigurationManager
+     */
+    protected $configurationManager;
+
+    /**
+     * @var ChainQueue
+     */
+    protected $chainQueue;
+
+    /**
+     * DrushCommand constructor.
+     * @param ConfigurationManager $configurationManager
+     * @param ChainQueue           $chainQueue
+     */
+    public function __construct(
+        ConfigurationManager $configurationManager,
+        ChainQueue $chainQueue
+    ) {
+        $this->configurationManager = $configurationManager;
+        $this->chainQueue = $chainQueue;
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('drush')
+            ->setDescription($this->trans('application.commands.drush.description'))
+            ->addArgument(
+                'command-name',
+                InputArgument::OPTIONAL,
+                $this->trans('commands.drush.arguments.command-name'),
+                null
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new DrupalStyle($input, $output);
+        $commandName = $input->getArgument('command-name');
+
+        $alternative = $this->configurationManager->readDrushEquivalents($commandName);
+
+        $io->info('Drupal Console equivalents');
+        $tableHeader = ['drush','drupal console'];
+
+        if (!$commandName && is_array($alternative)) {
+            $io->table(
+                $tableHeader,
+                $alternative
+            );
+
+            return 0;
+        }
+
+        $io->table(
+            $tableHeader,
+            [[$commandName, $alternative]]
+        );
+
+        if ($this->getApplication()->has($alternative)) {
+            $this->chainQueue->addCommand(
+                'help',
+                ['command_name' => $alternative]
+            );
+
+            return 0;
+        }
+
+        return 0;
+    }
+}
