a04b81392b59de756ee2036c06c40894e0bd7760
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2017-05-07 15:37:02 -0700

    [console] Remove constants.php file.

--- src/constants.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define("DRUPAL_CONSOLE_CORE", "/vendor/drupal/console-core/");
-define("DRUPAL_CONSOLE_LAUNCHER", "/vendor/drupal/console-launcher/");
-define("DRUPAL_CONSOLE", "/vendor/drupal/console/");
-define("DRUPAL_CONSOLE_LANGUAGE", "/vendor/drupal/console-%s/translations/");
