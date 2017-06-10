b733108bd1c562c8562405a71ee6c6f01fe38a01
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2017-06-10 13:06:52 -0700

    [console] Update ShowGenerateChainListener class.

--- src/EventSubscriber/ShowGenerateChainListener.php
+++ src/EventSubscriber/ShowGenerateChainListener.php
@@ -43,7 +43,8 @@ class ShowGenerateChainListener implements EventSubscriberInterface
     private $skipOptions = [
         'env',
         'generate-inline',
-        'generate-chain'
+        'generate-chain',
+        'no-interaction'
     ];
 
     /**
