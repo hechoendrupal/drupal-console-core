0dca9fd34cf60ce17621f4f1261739f2548ea951
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2017-06-22 14:02:43 -0700

    Drupal finder constants (#172)
    * [console] Fix package.
    
    * [console] Improve CONSTANT definition.

--- src/Utils/DrupalFinder.php
+++ src/Utils/DrupalFinder.php
@@ -18,30 +18,37 @@ class DrupalFinder extends DrupalFinderBase
 {
     public function locateRoot($start_path)
     {
+        $vendorDir = 'vendor';
         if (parent::locateRoot($start_path)) {
             $composerRoot = $this->getComposerRoot();
             $vendorDir = str_replace(
                 $composerRoot .'/', '', $this->getVendorDir()
             );
-            if (!defined("DRUPAL_CONSOLE_CORE")) {
-                define(
-                    "DRUPAL_CONSOLE_CORE",
-                    "/{$vendorDir}/drupal/console-core/"
-                );
-            }
-            if (!defined("DRUPAL_CONSOLE")) {
-                define("DRUPAL_CONSOLE", "/{$vendorDir}/drupal/console/");
-            }
-            if (!defined("DRUPAL_CONSOLE_LANGUAGE")) {
-                define(
-                    "DRUPAL_CONSOLE_LANGUAGE",
-                    "/{$vendorDir}/drupal/console-%s/translations/"
-                );
-            }
+
+            $this->defineConstants($vendorDir);
 
             return true;
         }
 
+        $this->defineConstants($vendorDir);
         return false;
+    }
+
+    protected function defineConstants($vendorDir) {
+        if (!defined("DRUPAL_CONSOLE_CORE")) {
+            define(
+                "DRUPAL_CONSOLE_CORE",
+                "/{$vendorDir}/drupal/console-core/"
+            );
+        }
+        if (!defined("DRUPAL_CONSOLE")) {
+            define("DRUPAL_CONSOLE", "/{$vendorDir}/drupal/console/");
+        }
+        if (!defined("DRUPAL_CONSOLE_LANGUAGE")) {
+            define(
+                "DRUPAL_CONSOLE_LANGUAGE",
+                "/{$vendorDir}/drupal/console-%s/translations/"
+            );
+        }
     }
 }
