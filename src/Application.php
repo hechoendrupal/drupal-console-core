7e8ebb0b7e0c31fb6b9b40ef5d442fe18b9d6c2e
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2017-07-09 17:44:43 -0700

    [console] Add mappings for debug commands. (#188)

--- src/Application.php
+++ src/Application.php
@@ -85,9 +85,9 @@ class Application extends BaseApplication
     /**
      * {@inheritdoc}
      */
-    public function doRun(InputInterface $input, OutputInterface $output)
-    {
+    public function doRun(InputInterface $input, OutputInterface $output) {
         $io = new DrupalStyle($input, $output);
+        $messages = [];
         if ($commandName = $this->getCommandName($input)) {
             $this->commandName = $commandName;
         }
@@ -103,14 +103,31 @@ class Application extends BaseApplication
             ->get('console.configuration_manager');
 
         if ($commandName && !$this->has($commandName)) {
-            $io->error(
-                sprintf(
-                    $this->trans('application.errors.invalid-command'),
-                    $this->commandName
-                )
-            );
+            $config = $configurationManager->getConfiguration();
+            $mappings = $config
+                ->get('application.commands.mappings');
 
-            return 1;
+            if (array_key_exists($commandName, $mappings)) {
+                $commandNameMap = $mappings[$commandName];
+                $messages['warning'][] = sprintf(
+                    $this->trans('application.errors.renamed-command'),
+                    $commandName,
+                    $commandNameMap
+                );
+                $this->add(
+                    $this->find($commandNameMap)->setAliases([$commandName])
+                );
+            }
+            else {
+                $io->error(
+                    sprintf(
+                        $this->trans('application.errors.invalid-command'),
+                        $this->commandName
+                    )
+                );
+
+                return 1;
+            }
         }
 
         $code = parent::doRun(
@@ -118,18 +135,31 @@ class Application extends BaseApplication
             $output
         );
 
-        if ($this->commandName != 'init' && $configurationManager->getMissingConfigurationFiles()) {
-            $io->warning($this->trans('application.site.errors.missing-config-file'));
+        if ($this->commandName != 'init' && $configurationManager->getMissingConfigurationFiles(
+            )
+        ) {
+            $io->warning(
+                $this->trans('application.site.errors.missing-config-file')
+            );
             $io->listing($configurationManager->getMissingConfigurationFiles());
             $io->commentBlock(
-                $this->trans('application.site.errors.missing-config-file-command')
+                $this->trans(
+                    'application.site.errors.missing-config-file-command'
+                )
             );
         }
 
-        if ($this->getCommandName($input) == 'list' && $this->container->hasParameter('console.warning')) {
+        if ($this->getCommandName(
+                $input
+            ) == 'list' && $this->container->hasParameter('console.warning')
+        ) {
             $io->warning(
                 $this->trans($this->container->getParameter('console.warning'))
             );
+        }
+
+        foreach ($messages as $type => $message) {
+            $io->$type($message);
         }
 
         return $code;
