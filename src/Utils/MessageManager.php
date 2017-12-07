3bc95edac85518b092cce6b1851c9b155f170685
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2017-12-07 14:36:19 -0800

    [console] Add remove method.

--- src/Utils/MessageManager.php
+++ src/Utils/MessageManager.php
@@ -19,41 +19,46 @@ class MessageManager
      * @param $type
      * @param $message
      * @param $code
+     * @param $isRemovable
      */
-    private function add($type, $message, $code)
+    private function add($type, $message, $code, $isRemovable)
     {
         $this->messages[] = [
             'type' =>$type,
             'message' => $message,
             'code' => $code,
+            'isRemovable' => $isRemovable
         ];
     }
 
     /**
      * @param $message
      * @param $code
+     * @param $isRemovable
      */
-    public function error($message, $code = 0)
+    public function error($message, $code = 0, $isRemovable = false)
     {
-        $this->add('error', $message, $code);
+        $this->add('error', $message, $code, $isRemovable);
     }
 
     /**
      * @param $message
      * @param $code
+     * @param $isRemovable
      */
-    public function warning($message, $code = 0)
+    public function warning($message, $code = 0, $isRemovable = false)
     {
-        $this->add('warning', $message, $code);
+        $this->add('warning', $message, $code, $isRemovable);
     }
 
     /**
      * @param $message
      * @param $code
+     * @param $isRemovable
      */
-    public function info($message, $code = 0)
+    public function info($message, $code = 0, $isRemovable = false)
     {
-        $this->add('info', $message, $code);
+        $this->add('info', $message, $code, $isRemovable);
     }
 
     /**
@@ -62,5 +67,30 @@ class MessageManager
     public function getMessages()
     {
         return $this->messages;
+    }
+
+    public function remove($type, $code = null) {
+        $this->messages = array_filter(
+            $this->messages,
+            function ($element) use ($type, $code)  {
+
+                if (!$element['isRemovable']) {
+
+                    return true;
+                }
+
+                if ($type != 'all' && $element['type'] != $type) {
+
+                    return true;
+                }
+
+
+                if (is_null($code)) {
+                    return false;
+                }
+
+                return !($element['code'] == $code);
+            }
+        );
     }
 }
