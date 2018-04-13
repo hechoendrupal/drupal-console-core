5337e5e9cb5c403bdd904d6a5181ca8a4b227ae8
Marcelo Vani <marcellovani@yahoo.co.uk>
2018-04-13 10:31:18 -0500

    Show the site name

--- src/Utils/ConfigurationManager.php
+++ src/Utils/ConfigurationManager.php
@@ -419,7 +419,9 @@ class ConfigurationManager
 
             foreach ($environments as $environment => $config) {
                 if (!array_key_exists('type', $config)) {
-                    throw new \UnexpectedValueException("The 'type' parameter is required in sites configuration.");
+                    throw new \UnexpectedValueException(sprintf(
+                      "The 'type' parameter is required in sites configuration:\n %s.", $site->getPathname())
+                    );
                 }
                 if ($config['type'] !== 'local') {
                     if (array_key_exists('host', $config)) {
